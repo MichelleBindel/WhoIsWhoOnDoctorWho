ec5bafbd2b7d062dae4f927c25d85e514da12663
MareikeJ <78210063+MareikeJ@users.noreply.github.com>
2021-03-04 10:44:02 +0100

    Script to create edges and node csv done...

--- structureScripts.R
+++ structureScripts.R
@@ -19,22 +19,27 @@ scriptsData <- subset(scriptsData, doctorid>8)
 #check if only doctors with id 9 and up are left
 scriptsData[!duplicated(scriptsData$doctorid), ]
 
-#remove details NA lines
+#remove lines that are neither talk nor location
 
 scriptsData <- subset(scriptsData, type=="talk" | type=="location")
 
+
+
+
+#compress speakers like Rose and Rose [OC] and Rose [on TV] to only one node of "ROSE"
 #change all "Doctor"s to their corresponding numbered Doctors
 for (row in 1:nrow(scriptsData)){
   doc <- scriptsData[row, "details"]
   if (!is.na(doc)){
+    doc <- str_remove_all(toString(doc), ' \\[.+\\]| \\[.+\\}|\\+|\\d+|\\(.+\\)')
+    scriptsData[row, "details"]<-doc
     if (doc=="DOCTOR"){
-      doctor <-c("Doctor")
-      doctor <- c(doctor, scriptsData[row, "doctorid"])
+      doctor <- c("DOCTOR", scriptsData[row, "doctorid"])
       scriptsData[row, "details"]<-toString(doctor)
     }
   }
 }
-rm(doc); rm(doctor); rm(doctorB); rm(row)
+rm(doc); rm(doctor); rm(row)
 
 #create non-duplicate list of all characters to create node file for Gephi
 charDF = distinct(na.omit(as.data.frame(scriptsData[, "details"])))
@@ -43,10 +48,16 @@ charDF <- cbind(id=id, charDF)
 write.csv(charDF,"nodes.csv", row.names = FALSE)
 
 
-#create dateframes for each episode
-#group by episode id
-#episodeGroups <- scriptsData %>% group_by(episodeid)
-#head(episodeGroups)
+#exchange speaker names with IDs through a lookup table (named vector)
+getSpeakerID <- charDF$id
+names(getSpeakerID) <- charDF$details
+
+for (row in 1:nrow(scriptsData)) {
+  IDfromLUT <- scriptsData[row, "details"]
+  scriptsData[row, "details"] <- getSpeakerID[toString(IDfromLUT)]
+}
+
+rm(IDfromLUT); rm(id); rm(getSpeakerID); rm(row)
 
 #split dataframe to episode-dataframes
 scriptData_byEpisode <- split(scriptsData, scriptsData$episodeid)
@@ -54,14 +65,12 @@ scriptData_byEpisode <- split(scriptsData, scriptsData$episodeid)
 #now we have 145 dataframes with the individual episodes inside
 
 episodesList <- list()
-speakersOverall <- c()
 
 #loop through episodes, for each episode do:
 for (i in scriptData_byEpisode) {
   
   sceneNum=0
   speakersInScene<-c(i[1, "details"])
-  #y=0 #end of scene
 
   episodeID=i[1, "episodeid"]
   
@@ -69,7 +78,7 @@ for (i in scriptData_byEpisode) {
   
   #loop through all rows of episode i
   for (row in 1:nrow(i)){
-    
+    speakersInScene <- c()
     #print(i[row, ])
     if (i[row, "type"]=="location") { #increase scene counter
       sceneNum = sceneNum+1
@@ -77,19 +86,42 @@ for (i in scriptData_byEpisode) {
     else { 
       speaker=i[row, "details"]
       if(!(speaker %in% speakersInScene)){
-      df = rbind(df, data.frame(episodeidCol=episodeID,sceneIdCol=sceneNum, idxCol=i[row, "idx"], nameOfSpeaker=speaker))
-      speakersInScene<-c(speakersInScene, speaker)
+        df = rbind(df, data.frame(episodeidCol=episodeID,sceneIdCol=sceneNum, idxCol=i[row, "idx"], nameOfSpeaker=speaker))
+        speakersInScene<-c(speakersInScene, speaker)
       }
-      }
+    }
   }
   #end of Episode,add df to episodes-list
  episodesList[[length(episodesList)+1]] <-df
  rm(speakersInScene)
 }
 
-rm(i);rm(df);rm(row);rm(sceneNum);rm(episodeID);rm(speaker) #remove unused variables
+rm(i);rm(df);rm(row);rm(sceneNum);rm(episodeID);rm(speaker); #remove unused variables
+combinations <- data.frame("Var1"=0, "Var2"=0)
 
-# TODO: - exchange name of speaker on episodesList with ID from charDF
-#       - create combinations of all characters within a scene
-#       - create DF with columns: source, target, type (undirected), weight (1?) with source/target for each combination of characters 
-#       - create edges.csv from DF
+
+# loop all scenes and create combination of all characters within one scene, creating all edges needed for the edge.csv
+for (i in episodesList){
+  
+  x <- split(i, i$sceneIdCol)
+
+  for (j in x) {
+    df <- expand.grid(j$details, j$details)
+    df <- df[!duplicated(df), ]
+    combinations <- rbind(combinations, df)
+    
+  }
+  
+}
+
+rm(df); rm(i); rm(j); rm(x)
+
+combinations <- combinations[order(combinations$Var1),]
+combinations <- aggregate(cbind(combinations[0],"weight"=1), combinations, length) # count how often combinations are present
+names(combinations)[1] <- "source"
+names(combinations)[2] <- "target"
+
+type <- rep("undirected", nrow(combinations))
+combinations <- cbind(combinations, type)
+write.csv(combinations,"edges.csv", row.names = FALSE)
+
