9a672a87fb9106899598a20421d642e5c7ff3ac9
MareikeJ <78210063+MareikeJ@users.noreply.github.com>
2021-03-13 09:34:16 +0100

    Add files via upload

--- structureScripts.R
+++ structureScripts.R
@@ -9,7 +9,24 @@ scriptsData <- read_csv("datasets/all-scripts.csv")
 
 #detailEpisodeData <- read_csv("datasets/all-detailsepisodes.csv")
 
-#imdbData <- read_csv("datasets/imdb_details.csv")
+imdbData <- read_csv("datasets/imdb_details.csv")
+
+# normalize season number according to script data
+imdbData$season[imdbData$season == 1] <- 27
+imdbData$season[imdbData$season == 2] <- 28
+imdbData$season[imdbData$season == 3] <- 29
+imdbData$season[imdbData$season == 4] <- 30
+imdbData$season[imdbData$season == 5] <- 31
+imdbData$season[imdbData$season == 6] <- 32
+imdbData$season[imdbData$season == 7] <- 33
+imdbData$season[imdbData$season == 8] <- 34
+imdbData$season[imdbData$season == 9] <- 35
+imdbData$season[imdbData$season == 10] <- 36
+imdbData$season[imdbData$season == 11] <- 37
+
+imdbData <- unite(imdbData, episode, c(season, number), remove=TRUE, sep = "-")
+
+imdbData = subset(imdbData, select = -c(title, nbr_votes, description))
 
 
 #remove scripts with doctors 1-8
@@ -19,7 +36,19 @@ scriptsData <- subset(scriptsData, doctorid>8)
 #check if only doctors with id 9 and up are left
 scriptsData[!duplicated(scriptsData$doctorid), ]
 
-#remove lines that are neither talk nor location
+scriptsData2 <- scriptsData[!duplicated(scriptsData$episodeid), ]
+
+
+scriptsData2 <- scriptsData2[ scriptsData2$episodeid %in% imdbData$episode, ]
+scriptsData <- scriptsData[ scriptsData$episodeid %in% imdbData$episode, ]
+
+imdbData <- imdbData[ imdbData$episode %in% scriptsData2$episodeid, ]
+
+rm(scriptsData2)
+
+#scale ratings to a scale of 0 to 1
+imdbData["rating"] <- (imdbData["rating"]-min(imdbData["rating"]))/(max(imdbData["rating"])-min(imdbData["rating"]))
+
 
 scriptsData <- subset(scriptsData, type=="talk" | type=="location")
 
@@ -41,10 +70,19 @@ for (row in 1:nrow(scriptsData)){
 }
 rm(doc); rm(doctor); rm(row)
 
+names(imdbData)[1] <- "episodeid"
+#double ratings so that low rated episodes lower the weight of the character combination and high rated episodes increase the weight
+imdbData$rating <- imdbData$rating * 2
+
+
+#add rating to scriptsData
+scriptsData <- merge(scriptsData[, c("idx", "text", "type", "details", "episodeid", "doctorid")], imdbData, by="episodeid")
+
 #create non-duplicate list of all characters to create node file for Gephi
 charDF = distinct(na.omit(as.data.frame(scriptsData[, "details"])))
 id <- rownames(charDF)
 charDF <- cbind(id=id, charDF)
+names(charDF)[2]<-paste("details")  # works
 write.csv(charDF,"nodes.csv", row.names = FALSE)
 
 
@@ -71,10 +109,10 @@ for (i in scriptData_byEpisode) {
   
   sceneNum=0
   speakersInScene<-c(i[1, "details"])
-
+  
   episodeID=i[1, "episodeid"]
   
-  df<-data.frame("episodeidCol"=episodeID,"sceneIdCol"=sceneNum, "idxCol"=i[1, "idx"], "nameOfSpeaker"=i[1, "details"])
+  df<-data.frame("episodeidCol"=episodeID,"sceneIdCol"=sceneNum, "idxCol"=i[1, "idx"], "nameOfSpeaker"=i[1, "details"], "rating"=i[1, "rating"])
   
   #loop through all rows of episode i
   for (row in 1:nrow(i)){
@@ -86,37 +124,79 @@ for (i in scriptData_byEpisode) {
     else { 
       speaker=i[row, "details"]
       if(!(speaker %in% speakersInScene)){
-        df = rbind(df, data.frame(episodeidCol=episodeID,sceneIdCol=sceneNum, idxCol=i[row, "idx"], nameOfSpeaker=speaker))
+        df = rbind(df, data.frame(episodeidCol=episodeID,sceneIdCol=sceneNum, idxCol=i[row, "idx"], nameOfSpeaker=speaker, rating=i[row, "rating"]))
         speakersInScene<-c(speakersInScene, speaker)
       }
     }
   }
   #end of Episode,add df to episodes-list
- episodesList[[length(episodesList)+1]] <-df
- rm(speakersInScene)
+  episodesList[[length(episodesList)+1]] <-df
+  rm(speakersInScene)
 }
 
 rm(i);rm(df);rm(row);rm(sceneNum);rm(episodeID);rm(speaker); #remove unused variables
+rm(scriptData_byEpisode); rm(scriptsData)
+
 combinations <- data.frame("Var1"=0, "Var2"=0)
+ratingsOverview <- data.frame("endOfRating" = 0, "rating" = 0)
 
-
-# loop all scenes and create combination of all characters within one scene, creating all edges needed for the edge.csv
-for (i in episodesList){
+# loop all scenes and create combination of all characters within one scene, creaing all edges needed for the edge.csv
+for (i in episodesList){ # loop episodes
   
   x <- split(i, i$sceneIdCol)
-
-  for (j in x) {
-    df <- expand.grid(j$details, j$details)
+  
+  for (j in x) { # loop scenes j of episode x
+    df <- expand.grid(j$nameOfSpeaker, j$nameOfSpeaker)
     df <- df[!duplicated(df), ]
     combinations <- rbind(combinations, df)
+    rm(df); rm(rating)
     
   }
-  
+  #save the ratings of the episodes for the combinations list
+  ratingsOverview[nrow(ratingsOverview) +1,] <- c(nrow(combinations), j[nrow(j), ]$rating)
 }
 
-rm(df); rm(i); rm(j); rm(x)
+rm(df); rm(i); rm(j); rm(x); rm(rating);# rm(imdbData)
 
-combinations <- combinations[order(combinations$Var1),]
+#combinations <- combinations[order(combinations$Var1),]
+combinationsRating <- combinations
+vectorOfRatings <- c(0)
+
+#vectorOfRatings <- c(vectorOfRatings, rep(ratingsOverview[2, 2], ratingsOverview[2, 1]))
+#vectorOfRatings <- c(vectorOfRatings, rep(ratingsOverview[3, 2], ratingsOverview[3, 1]-ratingsOverview[2, 1]))
+
+row2 = ratingsOverview[1,]
+
+for (i in 1:(nrow(ratingsOverview))) {
+  
+  row <- ratingsOverview[i,]
+  vectorOfRatings <- c(vectorOfRatings, rep(row[[2]], (row[[1]]-row2[[1]])))
+  row2<-row
+}
+rm(row); rm(row2)
+
+vectorOfRatings = vectorOfRatings[-1]
+combinationsRating <- cbind(combinationsRating, "ratings"=vectorOfRatings)
+
+combinationsRating2 <- combinationsRating %>% group_by(Var1, Var2) %>% summarise(weight=sum(ratings))
+combinationsRating2 = combinationsRating2[-1, ]
+combinationsRating2 <- combinationsRating2[combinationsRating2$weight > 5,]
+names(combinationsRating2)[1] <- "source"
+names(combinationsRating2)[2] <- "target"
+
+#combinationsWithout2 <- subset(combinations, select = -c(rating))
+#combinationsWithout2 <- ddply(combinationsWithout2,.(Var1,Var2),nrow)
+#combinationsWithout2 <- combinationsWithout2[combinationsWithout2$V1 > 5, ]
+
+#combinationsWithout <- subset(combinations, select = -c(rating))
+#591: 1-1 / 12693 obs ->
+#combinationsWithout <- aggregate(cbind(combinationsWithout[0],"weight"=1), combinationsWithout, length) # count how often combinations are present
+#combinationsWithout <- combinationsWithout[combinationsWithout$weight > 5, ]
+
+
+
+
+# edges list without Ratings, just combined number of occurences
 combinations <- aggregate(cbind(combinations[0],"weight"=1), combinations, length) # count how often combinations are present
 combinations <- combinations[combinations$weight > 5, ]
 names(combinations)[1] <- "source"
@@ -124,5 +204,9 @@ names(combinations)[2] <- "target"
 
 type <- rep("undirected", nrow(combinations))
 combinations <- cbind(combinations, type)
-write.csv(combinations,"edges.csv", row.names = FALSE)
+type <- rep("undirected", nrow(combinationsRating2))
+combinationsRating2 <- cbind(combinationsRating2, type)
+names(combinationsRating2)[4] <- "type"
 
+write.csv(combinations,"edges.csv", row.names = FALSE)
+write.csv(combinationsRating2,"edgesWithRatings.csv", row.names = FALSE)
